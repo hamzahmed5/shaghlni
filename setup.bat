ec7423b526d4356b7dfed2d11a643c6fc795b4c5
HaMzAS05 <hamzasaleem3105@gmail.com>
2026-04-10 22:39:59 +0300

    Auto database setup in bat file

--- setup.bat
+++ setup.bat
@@ -10,6 +10,7 @@ REM Check if Laragon is installed
 if not exist "C:\laragon\laragon.exe" (
     echo  [ERROR] Laragon is not installed!
     echo  Please download and install Laragon from: https://laragon.org/download/
+    echo  Install it then run this file again.
     echo.
     pause
     exit
@@ -21,28 +22,44 @@ xcopy /E /Y /Q "%~dp0*" "C:\laragon\www\jobpilot\" >nul
 echo  Done!
 
 echo.
-echo  [2/4] Starting Laragon...
+echo  [2/4] Starting Laragon services...
 start "" "C:\laragon\laragon.exe"
-timeout /t 5 /nobreak >nul
+timeout /t 6 /nobreak >nul
+
+REM Start Apache and MySQL via Laragon CLI
+"C:\laragon\bin\laragon\laragon.exe" startapache >nul 2>&1
+"C:\laragon\bin\laragon\laragon.exe" startmysql >nul 2>&1
+timeout /t 4 /nobreak >nul
 echo  Done!
 
 echo.
-echo  [3/4] Setting up the database...
-echo  Please do the following:
-echo.
-echo   1. Open Laragon and click "Start All"
-echo   2. Click "Database" to open HeidiSQL
-echo   3. Right-click on the left panel - Create new - Database
-echo   4. Name it: jobs_platform
-echo   5. Click on jobs_platform
-echo   6. Go to File - Run SQL file
-echo   7. Choose: C:\laragon\www\jobpilot\database\schema.sql
-echo.
-echo  Press any key when the database is ready...
-pause >nul
+echo  [3/4] Setting up database automatically...
+
+REM Find MySQL binary
+set MYSQL="C:\laragon\bin\mysql\mysql-8.4.3-winx64\bin\mysql.exe"
+
+if not exist %MYSQL% (
+    REM Try to find any MySQL version
+    for /d %%i in ("C:\laragon\bin\mysql\*") do set MYSQL="%%i\bin\mysql.exe"
+)
+
+REM Create database and import schema
+%MYSQL% -u root -e "CREATE DATABASE IF NOT EXISTS jobs_platform;" 2>nul
+%MYSQL% -u root jobs_platform < "C:\laragon\www\jobpilot\database\schema.sql" 2>nul
+
+if %errorlevel% == 0 (
+    echo  Done! Database created successfully!
+) else (
+    echo  [!] Database setup had an issue.
+    echo  Please manually import: C:\laragon\www\jobpilot\database\schema.sql
+    echo  into a database named: jobs_platform
+    echo.
+    pause
+)
 
 echo.
 echo  [4/4] Opening Shaghlni...
+timeout /t 2 /nobreak >nul
 start "" "http://localhost/jobpilot/frontend/index.html"
 
 echo.
